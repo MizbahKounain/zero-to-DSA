74e96e0840b3779d7e84b6947d3f74d419f48c82
Mizbah Kounain <mizbahkounain@gmail.com>
2026-07-28 20:25:08 +0530

    Added Patterns

---
+++ PATTERNS/pattern_02.java
@@ -0,0 +1,22 @@
+package PATTERNS;
+
+import java.util.Scanner;
+
+/*  ****
+    ***
+    **
+    *
+*/
+public class pattern_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        for (int i = row; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
