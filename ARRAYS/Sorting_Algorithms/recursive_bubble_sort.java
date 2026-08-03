4d527e195885f6b18561a8ce5830a0cbc8970e26
Mizbah Kounain <mizbahkounain@gmail.com>
2026-08-03 21:48:12 +0530

    Recursive bobble sort added

---
+++ ARRAYS/Sorting_Algorithms/recursive_bubble_sort.java
@@ -0,0 +1,39 @@
+package ARRAYS.Sorting_Algorithms;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class recursive_bubble_sort {
+    public static void sort(int[] arr, int n) {
+        if (n == 1)
+            return;
+        boolean swapped = false;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        // We can stop early if no swaps occur during a pass.
+        if (swapped == false)
+            return;
+        sort(arr, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+        sc.close();
+    }
+}
+
+// best com: O(n)
+// worst com: O(n^2)
